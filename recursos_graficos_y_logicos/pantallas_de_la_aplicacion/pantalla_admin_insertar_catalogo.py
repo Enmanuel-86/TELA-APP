1c39e3d2997e064d2ac19e13ed8b376b87c0684f
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2025-10-11 16:39:09 -0400

    Se creo esta pantalla para cargar las tablas catalogo  de la base de datos

---
+++ recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_admin_insertar_catalogo.py
@@ -0,0 +1,32 @@
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import  QWidget
+from PyQt5 import QtGui
+import os
+from ..elementos_graficos_a_py import Ui_PantallaInsertarCatalogoBD
+
+
+class PantallaAdminInsertarCatalogo(QWidget, Ui_PantallaInsertarCatalogoBD):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        
+        self.stacked_widget = stacked_widget
+        self.setupUi(self)
+        
+        
+        # Ruta relativa para las imagenes
+        self.boton_de_regreso.setIcon(QIcon.fromTheme(os.path.join(os.path.dirname(__file__), ".." ,"recursos_de_imagenes", "iconos_de_interfaz","flecha_izquierda_2.png")))
+        self.boton_registrar_cargo_empleado.setIcon(QIcon.fromTheme(os.path.join(os.path.dirname(__file__), ".." ,"recursos_de_imagenes", "iconos_de_interfaz","mas_blanco.png")))
+        self.boton_registrar_diagnostico.setIcon(QIcon.fromTheme(os.path.join(os.path.dirname(__file__), ".." ,"recursos_de_imagenes", "iconos_de_interfaz","mas_blanco.png")))
+        self.boton_registrar_enfermedad.setIcon(QIcon.fromTheme(os.path.join(os.path.dirname(__file__), ".." ,"recursos_de_imagenes", "iconos_de_interfaz","mas_blanco.png")))
+        self.boton_registrar_especialidad.setIcon(QIcon.fromTheme(os.path.join(os.path.dirname(__file__), ".." ,"recursos_de_imagenes", "iconos_de_interfaz","mas_blanco.png")))
+        self.boton_registrar_funcion_cargo.setIcon(QIcon.fromTheme(os.path.join(os.path.dirname(__file__), ".." ,"recursos_de_imagenes", "iconos_de_interfaz","mas_blanco.png")))
+        
+        
+        self.boton_de_regreso.clicked.connect(self.volver_atras)
+        
+    # Metodo para volver atras (pantalla de opciones admin)
+    def volver_atras(self):
+        self.stacked_widget.setCurrentIndex(7)
+    
+        
+    
