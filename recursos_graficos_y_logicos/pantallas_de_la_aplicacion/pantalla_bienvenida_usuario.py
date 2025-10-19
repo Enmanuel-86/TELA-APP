b4bf0e30678a531a014a9d487ce4b3d611498283
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2025-10-19 06:06:48 -0400

    Se borra la manera de insertar graficos de estadistica

---
+++ recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_bienvenida_usuario.py
@@ -0,0 +1,16 @@
+from PySide2.QtWidgets import (QWidget, QVBoxLayout, QPushButton)
+
+from ..elementos_graficos_a_py import  Ui_PantallaBienvenidaUsuario
+from datetime import datetime, time
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+import matplotlib.pyplot as plt
+
+class PantallaBienvenidaUsuario(QWidget, Ui_PantallaBienvenidaUsuario):
+    def __init__(self, stacked_widget):
+        super().__init__()
+
+        self.stacked_widget = stacked_widget
+        self.setupUi(self)
+        
+    
