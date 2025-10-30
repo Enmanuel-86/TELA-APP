3fc48a02252e93a488fd46fbd051b9fcd052ad8e
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2025-10-30 17:53:09 -0400

    Se empezo a cargar todas las lista a las vistas previas donde se ven lo catalogos
    NOTA
    
    hay que mejorar la manera de cargar las listas que retorna la base de datos

--- recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_admin_insertar_catalogo.py
+++ recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_admin_insertar_catalogo.py
@@ -14,6 +14,10 @@ from ..utilidades.funciones_sistema import FuncionSistema
 from repositorios.alumnos.alumno_repositorio import AlumnoRepositorio
 from repositorios.diagnosticos.diagnostico_repositorio import DiagnosticoRepositorio
 from repositorios.especialidades.especialidad_repositorio import EspecialidadRepositorio
+from repositorios.empleados.tipo_cargo_repositorio import TipoCargoRepositorio
+from repositorios.empleados.cargo_empleado_repositorio import CargoEmpleadoRepositorio
+from repositorios.empleados.funcion_cargo_repositorio import FuncionCargoRepositorio
+from repositorios.empleados.enfermedad_cronica_repositorio import EnfermedadCronicaRepositorio
 
 
 # Servicio
@@ -21,6 +25,10 @@ from repositorios.especialidades.especialidad_repositorio import EspecialidadRep
 from servicios.alumnos.alumno_servicio import AlumnoServicio
 from servicios.diagnosticos.diagnostico_servicio import DiagnosticoServicio
 from servicios.especialidades.especialidad_servicio import EspecialidadServicio
+from servicios.empleados.tipo_cargo_servicio import TipoCargoServicio
+from servicios.empleados.cargo_empleado_servicio import CargoEmpleadoServicio
+from servicios.empleados.funcion_cargo_servicio import FuncionCargoServicio
+from servicios.empleados.enfermedad_cronica_servicio import EnfermedadCronicaServicio
 
 
 ##################################
@@ -35,7 +43,13 @@ diagnostico_repositorio = DiagnosticoRepositorio()
 
 especialidad_repositorio = EspecialidadRepositorio()
 
+tipo_cargo_repositorio = TipoCargoRepositorio()
 
+cargo_empleado_repositorio = CargoEmpleadoRepositorio()
+
+funcion_cargo_repositorio = FuncionCargoRepositorio()
+
+enfermedad_cronica_repositorio = EnfermedadCronicaRepositorio()
 
 
 # Intancia de los servicio
@@ -46,9 +60,15 @@ diagnostico_servicio = DiagnosticoServicio(diagnostico_repositorio)
 
 especialidad_servicio = EspecialidadServicio(especialidad_repositorio)
 
+tipo_cargo_servicio = TipoCargoServicio(tipo_cargo_repositorio)
 
-lista_diagnosticos = diagnostico_servicio.obtener_todos_diagnosticos()
-lista_especialidades = especialidad_servicio.obtener_todos_especialidades()
+cargo_empleado_servicio = CargoEmpleadoServicio(cargo_empleado_repositorio)
+
+funcion_cargo_servicio = FuncionCargoServicio(funcion_cargo_repositorio)
+
+
+enfermedad_cronica_servicio = EnfermedadCronicaServicio(enfermedad_cronica_repositorio)
+
 
 
 class PantallaAdminInsertarCatalogo(QWidget, Ui_PantallaInsertarCatalogoBD):
@@ -67,24 +87,37 @@ class PantallaAdminInsertarCatalogo(QWidget, Ui_PantallaInsertarCatalogoBD):
         self.boton_registrar_funcion_cargo.setIcon(QIcon.fromTheme(os.path.join(os.path.dirname(__file__), ".." ,"recursos_de_imagenes", "iconos_de_interfaz","mas_blanco.png")))
         
         
+        self.lista_cargo = cargo_empleado_servicio.obtener_todos_cargos_empleados()
+        self.lista_tipo_cargo = tipo_cargo_servicio.obtener_todos_tipos_cargo()
+        self.lista_funcion_cargo = funcion_cargo_servicio.obtener_todos_funciones_cargo()
+        self.lista_diagnosticos = diagnostico_servicio.obtener_todos_diagnosticos()
+        self.lista_especialidades = especialidad_servicio.obtener_todos_especialidades()
+        self.lista_enfermerdades = enfermedad_cronica_servicio.obtener_todos_enfermedades_cronicas()
         
         
         
-        self.agregar_elementos_a_las_vistas_previas_catalogo(self.vista_previa_diagnosticos, lista_diagnosticos)
+        self.agregar_elementos_a_las_vistas_previas_catalogo(self.vista_previa_especialidades, self.lista_especialidades)
         
-        self.agregar_elementos_a_las_vistas_previas_catalogo(self.vista_previa_especialidades, lista_especialidades)
+        self.agregar_elementos_a_las_vistas_previas_catalogo(self.vista_previa_diagnosticos, self.lista_diagnosticos)
+        
+        self.agregar_elementos_a_las_vistas_previas_catalogo(self.vista_previa_enfermedades, self.lista_enfermerdades)
+
+        self.agregar_elementos_a_las_vistas_previas_catalogo(self.vista_previa_funciones_cargo, self.lista_funcion_cargo)
+        
+        
+        self.agregar_elementos_a_las_vistas_previas_catalogo(self.vista_previa_cargos_empleados, self.lista_cargo, 2)
 
         
         
    # Metodo para agregar diagnostico a la vista previa
-    def agregar_elementos_a_las_vistas_previas_catalogo(self, nombre_qlistwidget, lista_catalogo):
+    def agregar_elementos_a_las_vistas_previas_catalogo(self, nombre_qlistwidget, lista_catalogo, indice_elemento:int=1):
         
         i = 1
         for elemento_catalogo in lista_catalogo:
             
             
             
-            texto_a_mostrar = f"{i}) " + elemento_catalogo[1]
+            texto_a_mostrar = f"{i}) " + elemento_catalogo[indice_elemento]
             
             i += 1
             
@@ -140,23 +173,21 @@ class PantallaAdminInsertarCatalogo(QWidget, Ui_PantallaInsertarCatalogoBD):
             boton_editar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
             boton_editar.setFixedSize(40,40)
             boton_editar.setStyleSheet("""
-                                        
                                         QPushButton{
-                                            background: yellow;
-                                            border-radius:12px;
-                                            icon-size:28px;
-                                            border:1px solid black;
+
+                                        
+                                            
+                                            background-color: rgb(244, 131, 2);
+                                            
+                                            color: rgb(255, 255, 255);
                                         }
                                         
                                         QPushButton:hover{
                                             
-                                            background:#acacac
-                                            
-                                            
+                                            background-color: rgb(191, 64, 0);
                                         }
-                                        
-                                        
-                                        """)
+
+                                    """)
 
             # Botón para eliminar
             boton_borrar = QPushButton()
@@ -164,23 +195,21 @@ class PantallaAdminInsertarCatalogo(QWidget, Ui_PantallaInsertarCatalogoBD):
             boton_borrar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
             boton_borrar.setFixedSize(40,40)
             boton_borrar.setStyleSheet("""
-                                        
                                         QPushButton{
-                                            background:red;
-                                            border-radius:12px;
-                                            icon-size:28px;
-                                            border:1px solid black;
-                                        }
-                                        
-                                        QPushButton:hover{
-                                            
-                                            background:#acacac
-                                            
-                                            
-                                        }
+
                                         
                                         
-                                        """)
+                                        background-color: rgb(255, 0, 0);
+                                        
+                                        color: rgb(255, 255, 255);
+                                    }
+                                    
+                                    QPushButton:hover{
+                                    
+                                        background-color: rgb(147, 0, 0);
+                                    }
+                                    
+                                    """)
             
             row_layout.addWidget(boton_editar)
             row_layout.addWidget(boton_borrar)
