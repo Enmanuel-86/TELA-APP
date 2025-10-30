a751a87207a9afad917f23a3c9277a7f6337be97
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2025-10-30 05:24:23 -0400

    se empezo con la implementacion de las funciones de generar los documentos de los reportes
    ESTA EN PROCESO, ASI QUE TODAVIA FALTA, esto mas bien es un pequeño ensayo

--- recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_generar_informe_reportes_alumnos.py
+++ recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_generar_informe_reportes_alumnos.py
@@ -1,9 +1,25 @@
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import  QWidget
+from PyQt5.QtWidgets import  QWidget, QMessageBox
+from PyQt5.QtCore import QDate, QDateTime
+
 import os
 from ..elementos_graficos_a_py import Ui_PantallaGenerarInformesReportesAlumnos
 
 
+##################################
+# Importaciones de base de datos #
+##################################
+
+from repositorios.alumnos.asistencia_alumno_repositorio import AsistenciaAlumnoRepositorio
+from repositorios.especialidades.especialidad_repositorio import EspecialidadRepositorio
+from reportes.alumnos.reporte_asistencia_mensual_alumnos import ReporteAsistenciaMensualAlumnos
+from excepciones.base_datos_error import BaseDatosError
+
+# Instancia de la base de datos
+
+reporte_asistencia_mensual_alumnos = ReporteAsistenciaMensualAlumnos()
+asistencia_alumno_repositorio = AsistenciaAlumnoRepositorio()
+especialidad_repositorio = EspecialidadRepositorio()
 
 
 class PantallaGenerarInformesReportesAlumnos(QWidget, Ui_PantallaGenerarInformesReportesAlumnos):
@@ -13,6 +29,31 @@ class PantallaGenerarInformesReportesAlumnos(QWidget, Ui_PantallaGenerarInformes
         self.stacked_widget = stacked_widget
         self.setupUi(self)
         
-        # Rutas relativas
-        self.boton_de_regreso.setIcon(QIcon.fromTheme(os.path.join(os.path.dirname(__file__), "..","recursos_de_imagenes", "iconos_de_interfaz","flecha_izquierda_2.png")))
+        self.boton_generar_reporte_asistencia.clicked.connect(self.generar_reporte_de_asistencia)
         
+        
+    def generar_reporte_de_asistencia(self):
+        
+        
+        
+        ESPECIALIDAD_ID = 1
+        ANIO =  self.dateedit_fecha_reporte_asistencia.date().year()
+        MES = self.dateedit_fecha_reporte_asistencia.date().month()
+
+        # Se envuelve la carga de datos junto con la exportación para recibir cualquier error y que se muestre en pantalla en caso
+        # de que no se pueda exportar dicho reporte de asistencia
+        try:
+            # Acá se cargan los datos para que internamente se transporte hacia el siguiente método (exportar)
+            datos = reporte_asistencia_mensual_alumnos.cargar_datos(asistencia_alumno_repositorio, especialidad_repositorio, ESPECIALIDAD_ID, ANIO, MES)
+            
+            # Y por último exportamos los datos cargados
+            reporte_asistencia_mensual_alumnos.exportar(datos)
+        except BaseDatosError as error:
+            
+            QMessageBox.warning(self, "AVISO", f"{error}")
+            
+            
+        else:
+            
+            
+            QMessageBox.information(self, "AVISO", f"Generado con exito")
