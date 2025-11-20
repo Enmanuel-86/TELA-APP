43818c14c164509fbc7029e1e29ff447ee9ca31f
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2025-11-19 20:31:22 -0400

    Cambios:
    - Se estan implentando las 2 primeras funciones para generar reportes de alumnos
    - se organizo un poco el codigo
    - se documento un poco el codigo
    
    Falta:
    
    - otro metodo para generar el otro reporte necesario

--- recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_generar_informe_reportes_alumnos.py
+++ recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_generar_informe_reportes_alumnos.py
@@ -3,24 +3,60 @@ from PyQt5.QtWidgets import  QWidget, QMessageBox
 from PyQt5.QtCore import QDate, QDateTime
 
 import os
+import platform
 from ..elementos_graficos_a_py import Ui_PantallaGenerarInformesReportesAlumnos
+from ..utilidades.funciones_sistema import FuncionSistema
+from configuraciones.configuracion import app_configuracion
 
-
-"""##################################
+##################################
 # Importaciones de base de datos #
 ##################################
 
+# Reportes
+from reportes.alumnos.reporte_general_alumnos import ReporteGeneralAlumnos
+from excepciones.base_datos_error import BaseDatosError
+
+
+# Repositorios
+from repositorios.alumnos.alumno_repositorio import AlumnoRepositorio
+from repositorios.alumnos.medidas_alumno_repositorio import MedidasAlumnoRepositorio
+from repositorios.alumnos.inscripcion_repositorio import InscripcionRepositorio
+from repositorios.alumnos.info_clinica_alumno_repositorio import InfoClinicaAlumnoRepositorio
 from repositorios.alumnos.asistencia_alumno_repositorio import AsistenciaAlumnoRepositorio
 from repositorios.especialidades.especialidad_repositorio import EspecialidadRepositorio
 from reportes.alumnos.reporte_asistencia_mensual_alumnos import ReporteAsistenciaMensualAlumnos
-from excepciones.base_datos_error import BaseDatosError
+from repositorios.especialidades.especialidad_repositorio import EspecialidadRepositorio
+
+
+# Servicios
+from servicios.especialidades.especialidad_servicio import EspecialidadServicio 
+
 
 # Instancia de la base de datos
 
+# Reportes
 reporte_asistencia_mensual_alumnos = ReporteAsistenciaMensualAlumnos()
+
+reporte_general_alumnos = ReporteGeneralAlumnos()
+
+# Repositorios
 asistencia_alumno_repositorio = AsistenciaAlumnoRepositorio()
+
 especialidad_repositorio = EspecialidadRepositorio()
-"""
+
+alumno_repositorio = AlumnoRepositorio()
+
+medidas_alumno_repositorio = MedidasAlumnoRepositorio()
+
+inscripcion_repositorio = InscripcionRepositorio()
+
+info_clinica_alumno_repositorio = InfoClinicaAlumnoRepositorio()
+
+especialidad_repositorio = EspecialidadRepositorio()
+
+# Servicios
+especialidad_servicio = EspecialidadServicio(especialidad_repositorio)
+
 
 
 class PantallaGenerarInformesReportesAlumnos(QWidget, Ui_PantallaGenerarInformesReportesAlumnos):
@@ -29,32 +65,92 @@ class PantallaGenerarInformesReportesAlumnos(QWidget, Ui_PantallaGenerarInformes
         
         self.stacked_widget = stacked_widget
         self.setupUi(self)
-    """   
+
+        # Cargar lista catalogos
+        self.lista_especialidades = especialidad_servicio.obtener_todos_especialidades()
+        
+        # Cargar los elementos en el combobox
+        FuncionSistema.cargar_elementos_para_el_combobox(self.lista_especialidades, self.boton_especialidades, 1, 1)
+        
+        # cargar rutas de las carpetas de los documentos
+        self.carpeta_reportes_alumnos = str(app_configuracion.DIRECTORIO_REPORTES_ALUMNOS)
+        
+        # Establecer la fecha actual en el dateedit
+        self.dateedit_fecha_reporte_asistencia.setDate(QDate.currentDate())
+        
+        
+        # Conexiones a los botones
         self.boton_generar_reporte_asistencia.clicked.connect(self.generar_reporte_de_asistencia)
+        self.boton_generar_informe_integral_2.clicked.connect(self.generar_caraterizacion_general_alumno)
+        
         
         
     def generar_reporte_de_asistencia(self):
         
+        """
+            Este Metodo sirve para generar el reporte de asistencia que utilizan para calcular la estadistica de la asistencia de los alumnos segun su especialidad ocupacional
+        """
         
-        
-        ESPECIALIDAD_ID = 1
+        ESPECIALIDAD_ID = FuncionSistema.obtener_id_del_elemento_del_combobox(self.boton_especialidades, self.lista_especialidades, 1, 0, True)
         ANIO =  self.dateedit_fecha_reporte_asistencia.date().year()
         MES = self.dateedit_fecha_reporte_asistencia.date().month()
 
-        # Se envuelve la carga de datos junto con la exportación para recibir cualquier error y que se muestre en pantalla en caso
-        # de que no se pueda exportar dicho reporte de asistencia
+        
+        if not self.boton_especialidades.currentIndex() == 0:
+            # Se envuelve la carga de datos junto con la exportación para recibir cualquier error y que se muestre en pantalla en caso
+            # de que no se pueda exportar dicho reporte de asistencia
+            try:
+                # Acá se cargan los datos para que internamente se transporte hacia el siguiente método (exportar)
+                datos = reporte_asistencia_mensual_alumnos.cargar_datos(asistencia_alumno_repositorio, especialidad_repositorio, ESPECIALIDAD_ID, ANIO, MES)
+                
+                # Y por último exportamos los datos cargados
+                reporte_asistencia_mensual_alumnos.exportar(datos)
+                
+                
+                    
+            except BaseDatosError as error:
+                
+                QMessageBox.warning(self, "AVISO", f"{error}")
+                
+                
+            else:
+                
+                
+                QMessageBox.information(self, "AVISO", f"Generado con exito")
+                self.boton_especialidades.setCurrentIndex(0)
+                
+                
+                # 3. Verificar que existe
+                FuncionSistema.abrir_carpeta_contenedora_de_archivos(self.carpeta_reportes_alumnos)
+                
+
+        else:
+            
+            QMessageBox.warning(self, "AVISO", f"Seleccione una especialidad")
+            
+            
+    def generar_caraterizacion_general_alumno(self):
+        """
+        
+            Este Metodo sirve para generar el informe general de los alumnos que contiene toda la informacion relevante de cada alumno
+        
+        """
+        
         try:
-            # Acá se cargan los datos para que internamente se transporte hacia el siguiente método (exportar)
-            datos = reporte_asistencia_mensual_alumnos.cargar_datos(asistencia_alumno_repositorio, especialidad_repositorio, ESPECIALIDAD_ID, ANIO, MES)
             
-            # Y por último exportamos los datos cargados
-            reporte_asistencia_mensual_alumnos.exportar(datos)
+            datos = reporte_general_alumnos.cargar_datos(
+                inscripcion_repositorio, alumno_repositorio,
+                medidas_alumno_repositorio, info_clinica_alumno_repositorio
+            )
+            
+            reporte_general_alumnos.exportar(datos)
+            
         except BaseDatosError as error:
-            
-            QMessageBox.warning(self, "AVISO", f"{error}")
-            
+            print(error)
             
         else:
             
+            QMessageBox.information(self, "AVISO", f"Generado con exito")
             
-            QMessageBox.information(self, "AVISO", f"Generado con exito")"""
+            # Abrir carpeta contenedora de los archivos generados
+            FuncionSistema.abrir_carpeta_contenedora_de_archivos(self.carpeta_reportes_alumnos) 
