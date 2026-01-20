bfcd28f4bbe712a1389c0f7490a4a38ce774fe23
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-01-19 20:17:51 -0400

    se corrigieron algunas cosas que tienian al final del nombre "_2"

--- recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_vista_general_asistencia_empleados.py
+++ recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_vista_general_asistencia_empleados.py
@@ -5,6 +5,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5 import QtGui, QtCore
 import os
 from ..elementos_graficos_a_py import  Ui_VistaGeneralAsistenciaEmpleados
+from ..utilidades.funciones_sistema import FuncionSistema
 from datetime import (datetime, time, date)
 
 ##################################
@@ -46,3 +47,389 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
         self.stacked_widget = stacked_widget
         self.setupUi(self)
       
+        # variable para contar las asistencias
+        self.contador_de_asistencias = 0
+        
+        # Label que muestra el conteo de asistencias
+        self.label_titulo_asistencia.setText(f"Lista actual de asistencias: {self.contador_de_asistencias}")
+
+        # lista para almacenar los empleados que se van agregando a la lista de asistencias
+        self.lista_de_asistencias = []
+        
+        # lista de empleados actuales en la bd
+        self.lista_empleados_actual = []
+        
+        # esta lista guardara los empleados que se agreguen a la lista de asistencias
+        self.lista_agregados = []
+        
+        # esto es para el metodo de agregar al empleado a la lista 
+        self.indice = 0
+
+        self.lista_qlineedits = (self.input_cedula_empleado, self.input_motivo_retraso, self.input_motivo_inasistencia)
+        self.lista_radiobuttons = (self.radioButton_asistente, self.radioButton_inasistente)
+        self.lista_timeedits = (self.timeEdit_hora_entrada, self.timeEdit_hora_salida)
+        
+
+        self.msg_box = QMessageBox(self)
+        
+        # Crear botones personalizados
+        self.boton_si = self.msg_box.addButton("Sí", QMessageBox.YesRole)
+        self.boton_no = self.msg_box.addButton("No", QMessageBox.NoRole)
+
+        
+        self.actualizar_lista_busqueda()
+        
+        self.input_cedula_empleado.textChanged.connect(self.filtrar_resultados)
+        self.boton_crear_registro.clicked.connect(lambda: self.ventanas_registro_asistencia.setCurrentIndex(1))
+        self.boton_agregar.clicked.connect(lambda: self.agregar_info())
+        self.dateedit_fecha_asistencia.setDate(QDate.currentDate())
+        
+        # Lista de coincidencias
+        self.resultados = QListWidget(self)
+        self.resultados.setFocusPolicy(Qt.NoFocus)
+        self.resultados.setMouseTracking(True)
+        self.resultados.setStyleSheet("padding:10px;")
+        self.resultados.itemClicked.connect(self.seleccionar_item)
+        self.resultados.hide() 
+        
+        
+        
+    def actualizar_lista_busqueda(self):
+        
+        self.lista_empleados_actual = empleado_servicio.obtener_todos_empleados()
+        
+    
+    #################################################################################
+    # Metodos para la barra de busqueda (el QLineedit de la cedula del empleado)
+    
+    def filtrar_resultados(self, texto):
+        texto = texto.strip().lower()
+        self.resultados.clear()
+
+        if not texto:
+            self.resultados.hide()
+            self.label_nombre_empleado_guia.clear()
+            return
+        
+    
+        
+        coincidencias = [
+            persona for persona in self.lista_empleados_actual
+            if texto in persona[6] or texto in persona[1].lower()
+        ]
+
+        if not coincidencias:
+            self.resultados.hide()
+            return
+
+        for persona in coincidencias:
+            item = f'{persona[6]} - {persona[1]} {persona[4]}'
+            
+            global nombre_guia
+            nombre_guia = f" {persona[1].capitalize()} {persona[4].capitalize()}"
+            
+            self.resultados.addItem(QListWidgetItem(item))
+
+        # Ocultar si hay una coincidencia exacta por cédula
+        if len(coincidencias) == 1 and coincidencias[0][1] == texto:
+            self.resultados.hide()
+        else:
+            self.mostrar_lista()
+            self.label_nombre_empleado_guia.clear()
+
+
+    def mostrar_lista(self):
+        pos = self.input_cedula_empleado.mapTo(self, QPoint(0, self.input_cedula_empleado.height()))
+        self.resultados.move(pos)
+        self.resultados.resize(self.input_cedula_empleado.width(), 100)
+        self.resultados.show()
+        
+
+    def seleccionar_item(self, item):
+        cedula = item.text().split(" - ")[0]
+        self.input_cedula_empleado.setText(cedula)
+        self.label_nombre_empleado_guia.setText(nombre_guia)
+        self.resultados.hide()
+        
+    #################################################################################
+    
+    
+    # Metodo para agregar la informacion a la lista de asistencias
+    def agregar_info(self):
+        
+        try: 
+            
+            if self.radioButton_asistente.isChecked() == False and self.radioButton_inasistente.isChecked() == False:
+                
+                QMessageBox.warning(self, "Error", "Por favor, seleccione si el empleado asistió o no.")
+                
+                
+                return
+            
+            else:
+                empleado_n = []
+                
+                cedula = self.input_cedula_empleado.text().strip()
+                empleado_id = FuncionSistema.buscar_id_por_cedula(cedula, self.lista_empleados_actual)
+                
+                
+                if not any(cedula in empleado[6] for empleado in self.lista_empleados_actual):
+                    
+                    QMessageBox.warning(self, "Error", "La persona ya esta agregada o no existe")
+                    return
+                
+                
+                empleado = empleado_servicio.obtener_empleado_por_id(empleado_id)
+                
+                
+                            
+                
+                # la lista se compone de: id_empleado, fecha_asistencia, hora_entrada, hora_salida,
+                # estado_asistencia, motivo_retraso, motivo_inasistencia, nombre y apellido del empleado 
+                # (estas ultimas son para mostrar nada mas)
+                
+                # 0) Empleado ID
+                empleado_n.append(empleado_id)
+                
+                # 1) Fecha de asistencia
+                dia_actual = date(self.dateedit_fecha_asistencia.date().year(), self.dateedit_fecha_asistencia.date().month(), self.dateedit_fecha_asistencia.date().day())
+
+                empleado_n.append(dia_actual)
+                
+                # 2) Hora de entrada
+                
+                
+                hora_entrada = (self.timeEdit_hora_entrada.time().hour() , self.timeEdit_hora_entrada.time().minute())
+
+                empleado_n.append(hora_entrada)
+                
+                # 3) Hora de salida
+                hora_salida = (self.timeEdit_hora_salida.time().hour() , self.timeEdit_hora_salida.time().minute())
+                empleado_n.append(hora_salida)
+                
+                # 4) Estado de asistencia
+                if self.radioButton_asistente.isChecked():
+                    estado_asistencia = "PRESENTE"
+                    empleado_n.append(estado_asistencia)
+                
+                elif self.radioButton_inasistente.isChecked():
+                    estado_asistencia = "AUSENTE"
+                    empleado_n.append(estado_asistencia)
+                    
+                    
+                # 5) Motivo de retraso
+                if self.input_motivo_retraso.text().strip():
+                    motivo_retraso = self.input_motivo_retraso.text().strip()
+                    empleado_n.append(motivo_retraso)
+                else:
+                    empleado_n.append(None)
+                    
+                    
+                # 6) Motivo de inasistencia
+                if self.input_motivo_inasistencia.text().strip():
+                    motivo_inasistencia = self.input_motivo_inasistencia.text().strip()
+                    empleado_n.append(motivo_inasistencia)
+                else:
+                    empleado_n.append(None)
+                
+                # 7) cedula
+                empleado_n.append(cedula)
+                
+                #8) nombre
+                empleado_n.append(empleado[1])
+                
+                #9) apellido
+                empleado_n.append(empleado[4])
+                
+                
+                # el texto que quiero mostrar en la lista de asistencias es : Cedula - Nombre Apellido
+                texto_a_mostrar = f"{empleado_n[7]} - {empleado_n[8]} {empleado_n[9]}"
+                
+                
+                empleado_n = tuple(empleado_n)
+                
+                empleado = empleado_servicio.obtener_empleado_por_id(empleado_n[0])
+                
+                print(empleado_n)
+                
+                
+                    
+                
+                
+                
+                if not len(self.lista_de_asistencias) > 0:
+                
+                    # Agregamos los elementos al "carrito"
+                    if not empleado_n in self.lista_de_asistencias:
+                        
+                        
+                        
+                        self.eliminar_empleado_de_lista(empleado)
+                        
+                        print(empleado_n)
+                        # si no esta los agrega a la lista de asistencia
+                        self.lista_de_asistencias.append(empleado_n)
+                        
+                        
+                        # lo agrega al "carrito"
+                        self.agg_empleado_a_lista(self.lista_asistencias_en_cola, self.lista_de_asistencias, self.input_cedula_empleado, texto_a_mostrar)
+
+                        # Limpiamos los inputs
+                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons, (), self.lista_timeedits)
+                        
+                        
+                        
+                        
+                        
+                    
+                    else:
+                        
+                        QMessageBox.warning(self, "Error", "Persona ya agregada")
+                        # Limpiamos los inputs
+                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons, (), self.lista_timeedits)
+                        
+                        return
+                    
+                else:
+                    
+                    # Agregamos los elementos al "carrito"
+                    if not empleado_n[7] in self.lista_de_asistencias[self.indice][7] or not empleado_n[7] == self.lista_de_asistencias[self.indice][7]:
+                        
+                        # si no esta los agrega a la lista de asistencia
+                        self.lista_de_asistencias.append(empleado_n)
+                        
+                        
+                        self.eliminar_empleado_de_lista(empleado)
+                        
+                        # lo agrega al "carrito"
+                        self.agg_empleado_a_lista(self.lista_asistencias_en_cola, self.lista_de_asistencias, self.input_cedula_empleado, texto_a_mostrar)
+
+                        # Limpiamos los inputs
+                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons, (), self.lista_timeedits)
+                        
+                        # y le sumamos uno al indice para saber la posicion actual
+                        self.indice += 1
+                        
+                    
+                    else:
+                        
+                        QMessageBox.warning(self, "Error", "Persona ya agregada")
+                        # Limpiamos los inputs
+                        self.limpiar_inputs(self.lista_qlineedits, self.lista_radiobuttons, self.lista_timeedits)
+                        
+                        return
+                    
+                    
+                    
+                
+                    
+                
+                # esto es para actualizar cuantas personas hay 
+                self.contador_de_asistencias += 1
+                self.label_titulo_asistencia.setText(f"Lista actual de asistencias: {self.contador_de_asistencias}")
+                
+                
+
+        except Exception as e:
+            print(f"Error al agregar la informacion: {e}")
+            
+            
+    
+    def eliminar_empleado_de_lista(self, empleado):
+        
+        """
+            Metodo para eliminar al empleado de la lista en donde estan todos los empleados actuales
+            esto es para que cuando agregue un empleado la lista de la barra de busqueda no muestre el 
+            empleado que ya fue agregado a la lista de asistencias
+        """
+        if empleado in self.lista_empleados_actual:
+                            
+            self.lista_agregados.append(empleado)
+            
+            self.lista_empleados_actual.remove(empleado)
+            
+            
+    
+    def agg_empleado_a_lista(self, nombre_qlistwidget, nombre_lista, enfoca_input, texto_a_mostrar=None):
+        
+        """
+            Metodo "carrito" para agregar a los empleados a la lista de asistencia
+        """
+        
+        
+        # Crear un QListWidgetItem
+        item = QListWidgetItem()
+        nombre_qlistwidget.addItem(item)
+        
+        
+
+        # Crear un widget personalizado para la fila
+        widget = QWidget()
+        row_layout = QHBoxLayout()
+        widget.setLayout(row_layout)
+
+        # Label para el texto
+        if self.radioButton_asistente.isChecked():
+            
+                
+            label = QLabel(texto_a_mostrar if texto_a_mostrar else f"Elemento {self.list_widget.count() + 1}")
+            label.setProperty("tipo", "asistente")
+            row_layout.addWidget(label)
+            
+            
+        
+        elif self.radioButton_inasistentee.isChecked():
+            
+            label = QLabel(texto_a_mostrar if texto_a_mostrar else f"Elemento {self.list_widget.count() + 1}")
+            label.setProperty("tipo", "inasistente")
+            row_layout.addWidget(label)
+
+        # Botón para eliminar
+        boton_borrar = QPushButton()
+        boton_borrar.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        boton_borrar.setFixedSize(60,40)
+        boton_borrar.setIconSize(QSize(30, 30))
+        boton_borrar.setProperty("tipo","boton_borrar")
+        
+        
+        boton_borrar.clicked.connect(lambda: self.borrar_elementos_a_la_vista_previa(nombre_qlistwidget, nombre_lista, enfoca_input, item))
+        row_layout.addWidget(boton_borrar)
+
+        # Asignar el widget al QListWidgetItem
+        item.setSizeHint(widget.sizeHint())
+        nombre_qlistwidget.setItemWidget(item, widget)
+        
+        
+        
+    def borrar_elementos_a_la_vista_previa(self, nombre_qlistwidget, nombre_lista, enfoca_input,  item):
+        
+        
+        # Logica para borrar el registro del diagnostico de la lista
+        
+        # indice del listwidget
+        indice_vista_previa = nombre_qlistwidget.row(item)
+        
+        # borramos el elemento de la lista segun el indice del listwidget
+        del nombre_lista[indice_vista_previa]
+        self.indice -= 1
+        
+        # darle foco al input del segmento
+        # esto lo hice porque al borrar toda la lista de X segmento, esta se subia al arriba del todo del formulario
+        enfoca_input.setFocus()
+        
+        if not self.lista_agregados[indice_vista_previa] in self.lista_empleados_actual:
+            
+            empleado_restaurar = self.lista_agregados[indice_vista_previa]
+            
+            self.lista_empleados_actual.append(empleado_restaurar)
+            
+            self.lista_agregados.remove(empleado_restaurar)
+        
+        
+        # actualizar el contador de asistencias
+        self.contador_de_asistencias -= 1
+        self.label_titulo_asistencia.setText(f"Lista actual de asistencias: {self.contador_de_asistencias}")
+        
+        
+
+            
