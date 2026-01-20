aecaef3eedd62c7d889de652b3f5b505644f0235
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2026-01-19 20:55:14 -0400

    ahora se puede registrar a los empleados que se fueron agregando el qlistwidget que muestra a quien se esta agregando

--- recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_vista_general_asistencia_empleados.py
+++ recursos_graficos_y_logicos/pantallas_de_la_aplicacion/pantalla_vista_general_asistencia_empleados.py
@@ -83,6 +83,7 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
         self.boton_crear_registro.clicked.connect(lambda: self.ventanas_registro_asistencia.setCurrentIndex(1))
         self.boton_agregar.clicked.connect(lambda: self.agregar_info())
         self.dateedit_fecha_asistencia.setDate(QDate.currentDate())
+        self.boton_suministrar.clicked.connect(lambda: self.suministrar_asistencias())
         
         # Lista de coincidencias
         self.resultados = QListWidget(self)
@@ -199,12 +200,12 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
                 # 2) Hora de entrada
                 
                 
-                hora_entrada = (self.timeEdit_hora_entrada.time().hour() , self.timeEdit_hora_entrada.time().minute())
+                hora_entrada = time(self.timeEdit_hora_entrada.time().hour() , self.timeEdit_hora_entrada.time().minute())
 
                 empleado_n.append(hora_entrada)
                 
                 # 3) Hora de salida
-                hora_salida = (self.timeEdit_hora_salida.time().hour() , self.timeEdit_hora_salida.time().minute())
+                hora_salida = time(self.timeEdit_hora_salida.time().hour() , self.timeEdit_hora_salida.time().minute())
                 empleado_n.append(hora_salida)
                 
                 # 4) Estado de asistencia
@@ -275,7 +276,7 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
                         self.agg_empleado_a_lista(self.lista_asistencias_en_cola, self.lista_de_asistencias, self.input_cedula_empleado, texto_a_mostrar)
 
                         # Limpiamos los inputs
-                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons, (), self.lista_timeedits)
+                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons)
                         
                         
                         
@@ -286,7 +287,7 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
                         
                         QMessageBox.warning(self, "Error", "Persona ya agregada")
                         # Limpiamos los inputs
-                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons, (), self.lista_timeedits)
+                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons)
                         
                         return
                     
@@ -305,7 +306,7 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
                         self.agg_empleado_a_lista(self.lista_asistencias_en_cola, self.lista_de_asistencias, self.input_cedula_empleado, texto_a_mostrar)
 
                         # Limpiamos los inputs
-                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons, (), self.lista_timeedits)
+                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons)
                         
                         # y le sumamos uno al indice para saber la posicion actual
                         self.indice += 1
@@ -315,14 +316,17 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
                         
                         QMessageBox.warning(self, "Error", "Persona ya agregada")
                         # Limpiamos los inputs
-                        self.limpiar_inputs(self.lista_qlineedits, self.lista_radiobuttons, self.lista_timeedits)
+                        FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons)
+                        self.timeEdit_hora_entrada.setTime(QTime(7, 0))  
+                        self.timeEdit_hora_salida.setTime(QTime(12, 0)) 
                         
                         return
                     
                     
                     
                 
-                    
+                self.timeEdit_hora_entrada.setTime(QTime(7, 0))  
+                self.timeEdit_hora_salida.setTime(QTime(12, 0))  
                 
                 # esto es para actualizar cuantas personas hay 
                 self.contador_de_asistencias += 1
@@ -378,7 +382,7 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
             
             
         
-        elif self.radioButton_inasistentee.isChecked():
+        elif self.radioButton_inasistente.isChecked():
             
             label = QLabel(texto_a_mostrar if texto_a_mostrar else f"Elemento {self.list_widget.count() + 1}")
             label.setProperty("tipo", "inasistente")
@@ -404,7 +408,7 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
     def borrar_elementos_a_la_vista_previa(self, nombre_qlistwidget, nombre_lista, enfoca_input,  item):
         
         
-        # Logica para borrar el registro del diagnostico de la lista
+        # Logica para borrar el empleado del QListWidget
         
         # indice del listwidget
         indice_vista_previa = nombre_qlistwidget.row(item)
@@ -430,6 +434,128 @@ class PantallaVistaGeneralAsistenciaEmpleados(QWidget, Ui_VistaGeneralAsistencia
         self.contador_de_asistencias -= 1
         self.label_titulo_asistencia.setText(f"Lista actual de asistencias: {self.contador_de_asistencias}")
         
+        ##########################################################################
+        # Obtener la fila del item y eliminarlo
+        row = nombre_qlistwidget.row(item)
+        nombre_qlistwidget.takeItem(row)
+    
+        print(f"lista actualizada: {nombre_lista}")
+        
+        
+    # Metodo para suministrar las asistencias a la base de datos
+    def suministrar_asistencias(self):
+        
+        try:
+            
+             # Indice solo para ver el numero de iteraciones
+            indice = 0
+            
+            
+            self.msg_box.setWindowTitle("Confirmar registro")
+            self.msg_box.setText("¿Seguro que quiere registrar esta lista de asistencia?")
+            self.msg_box.setIcon(QMessageBox.Question)
+            QApplication.beep()
+
+
+
+            # Mostrar el cuadro de diálogo y esperar respuesta
+            self.msg_box.exec_()
+
+            if self.msg_box.clickedButton() == self.boton_si:
+                
+                
+                
+                
+                # Iteramos cada empleado que esta en la lista de asistencia
+                for empleado in self.lista_de_asistencias:
+                    
+                    # esto es para ver en la consola
+                    print(f"Empleado {indice} ID:{empleado[0]} Nombre:{empleado[8]}")
+                    indice += 1
+                    
+                        
+                        
+                    empleado_id = empleado[0]
+                    
+                    fecha_asistencia = empleado[1]
+                    
+                    hora_entrada = empleado[2]
+                    
+                    
+                    hora_salida = empleado[3]
+                    
+                    estado_asistencia = empleado[4]
+                    
+                    motivo_retraso = empleado[5]
+                    
+                    motivo_inasistencia = empleado[6]
+                    
+                    
+                    
+                    
+                    campos_asistencia_empleados = {
+                                "empleado_id": empleado_id,
+                                "fecha_asistencia": fecha_asistencia,
+                                "hora_entrada": hora_entrada,
+                                "hora_salida": hora_salida,
+                                "estado_asistencia": estado_asistencia,
+                                "motivo_retraso": motivo_retraso,
+                                "motivo_inasistencia": motivo_inasistencia
+                            }
+                    
+                    errores_totales = asistencia_empleado_servicio.validar_asistencia_empleado(
+                                        campos_asistencia_empleados.get("empleado_id"), 
+                                        campos_asistencia_empleados.get("fecha_asistencia"),
+                                        campos_asistencia_empleados.get("estado_asistencia"),
+                                        campos_asistencia_empleados.get("motivo_retraso"),
+                                        campos_asistencia_empleados.get("motivo_inasistencia")
+                                        )
+                    
+                    
+                    if errores_totales:
+                        
+                        QMessageBox.information(self, "Error", "Tu registro de asistencia a tenido un error. ")
+                        estado = False
+                        return
+                    else:
+                        asistencia_empleado_servicio.registrar_asistencia_empleado(campos_asistencia_empleados)
+                        estado = True
+                        
+                        
+                if estado:
+                    
+                    QMessageBox.information(self, "Registro exitoso", "Tu registro de asistencia a sido exitoso. ")
+                    FuncionSistema.limpiar_inputs_de_qt(self.lista_qlineedits, self.lista_radiobuttons)
+                    
+                    # Limpiamos las listas y los contadores
+                    self.lista_asistencias_en_cola.clear()
+                    self.lista_de_asistencias.clear()
+                    self.lista_agregados.clear()
+                    self.indice = 0
+                    self.contador_de_asistencias = 0
+                    
+                    # restablecemos el contador de asistencias
+                    self.label_titulo_asistencia.setText(f"Lista actual de asistencias: {self.contador_de_asistencias}")
+                    
+                    # restablecemos la lista de empleados actuales de la bd
+                    self.actualizar_lista_busqueda()
+
+                    
+                    
+                    
+                        
+            elif self.msg_box.clickedButton() == self.boton_no:
+                
+                return
+                    
+                    
+            
+        except Exception as e:
+            
+            print(f"algo malo paso en suministrar info : {e}")
+                                    
+                                    
+                                 
         
 
             
